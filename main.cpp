43bea60fa6f2279c06a86e3d34db59ad1fd9d4a5
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-270.local>
2025-11-05 15:21:38 -0800

    Working on Psuedocode

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// Include necesssary headers for file handling, data structures, etc.
+
+// Define a function to simulate astronomical changes over time
+
+// Parameters: map of solar system zones, number of time intervals
+
+// Define main function
+// Initialize a mpa to store solar system zone info, each associoated with an
+// array of lists for planets, stars, and black holes
+
+// Open an external file to read data about solar system zones and to populate
+// the map If file does not open, print an error and exit
+
+// Read data from the file and populate the map
+// For each line, extract solar system zone and celestial body data
+// Each line contains zone, category, and event
+
+// Insert celestial bodies into the appropriate list in the array for their
+// solar system zone
+
+// Close the file
+
+// Begin a time-based simulation for astronmical changes
